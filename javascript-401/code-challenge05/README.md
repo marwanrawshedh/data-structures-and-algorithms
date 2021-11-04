b58af9b0d8e83b36c972aa5d7ee016733f0c8147
marwanrawshedh <marwansalem55@yahoo.com>
2021-11-04 21:25:22 +0200

    code ch05

---
+++ javascript-401/code-challenge05/README.md
@@ -0,0 +1,26 @@
+# Singly Linked List
+<!-- Short summary or background information -->
+i created a Singly Linked List include a value and net point with three methode allow me to insert node search for a value reprsent all list's values .
+
+## Challenge
+<!-- Description of the challenge -->
+## this challenge is asking to create a Singly Linked Lists that contain a insert method (which allows us to add a new node ),includes method (This allows us to search for a value and return true if it exists or false if it isn't)and a toString method (that return a string representing all the values in the Linked List)
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+- Singly Linked Lists why? it is a simple data structur  Big O =O(n)
+## API
+- insert method: it is a method take a value which allows us to Add a new node with that value to the head of the list with an O(1) Time performance. 
+- includes method:it is a method take a value which  allows us to search for a value and return true if  exist or false if it is not
+- toString method:it is a method doesnt take any Arguments which return a string representing all the values in the Linked List as a string formatted as:"{ a } -> { b } -> { c } -> NULL".
+
+- [x] Can successfully instantiate an empty linked list
+- [x] Can properly insert into the linked listVenus
+- [x] The head property will properly point to the first node in the linked list
+- [x] Can properly insert multiple nodes into the linked list
+- [x] Will return true when finding a value within the linked list that exists
+- [x] Will return false when searching for a value in the linked list that does not exist
+- [x] Can properly return a collection of all the values that exist in the linked list
+
+
+
+
---
+++ javascript-401/code-challenge05/lib/linkedList.js
@@ -0,0 +1,95 @@
+"use strict";
+const Node = require("./node");
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  /**
+   * A function that will append a node to the end of the linkedList
+   *
+   * @param {any} value
+   */
+  append(value) {
+    // Algorithm
+    // create a new node
+    const newNode = new Node(value);
+
+    // check if the linked list is empty
+    if (!this.head) {
+      // if empty, point that linked list to be the head of the linked list
+      this.head = newNode; // make the newly created node our first node in our Linked List
+    
+      return this; // return the linkedList
+    }
+    
+    // if its not empty then you need to find the last node, or the node that the next value is null.
+    let currentNode = this.head; //this will be our pointer, indicator while we ware traversing through the linked List
+    // traverse the Linked list and  find the node that the next value of it is null (traversing)
+// console.log(value)
+    while (currentNode.next) {
+      // while the next node is not empty, make the current node move to th next node
+      currentNode = currentNode.next;
+    }
+    // once found, link that node to the newly created node
+   
+    currentNode.next = newNode;
+
+    // return the LinkedList
+    
+    return this;
+  }
+
+  
+  insert(value) {
+    const newnode = new Node(value);
+    if (!this.head) {
+      this.head = newnode;
+      return this;
+    }
+    return this;
+  }
+
+  includes(value) {
+    let currntNode = this.head;
+    let boolean = false;
+    if(currntNode){ // if condition to prevent from error check if the linked list is empty
+    let currntValue = this.head.value;
+    while (currntNode) {
+      if (currntValue == value) { //check if we the target number existing
+        boolean = true;
+        currntNode = null;
+        return boolean;
+      }
+      currntNode = currntNode.next;
+      if (currntNode) { //check if we reached the last node
+        currntValue = currntNode.value;
+      }
+    }}
+    return boolean;
+  }
+
+
+  toString() {
+    let currntNode = this.head;
+    let string = "";
+    if(this.head){ // if condition to prevent from error check if the linked list is empty 
+    let currntValue = this.head.value;
+    
+    while (currntNode ) { 
+     
+     
+      string+=`{ ${ currntValue } } -> `
+
+      currntNode = currntNode.next;
+      if (currntNode) { //check if we reached the last node
+        currntValue = currntNode.value;
+      } else {string+="null"}
+    }
+  }
+  return string;
+}
+}
+
+module.exports = LinkedList;
